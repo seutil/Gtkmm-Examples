e6dc58df9bed104f4f908d3e3bd29ce0c5ea5a0b
seutil <seutil@inbox.ru>
2023-08-19 22:54:17 +0300

    remove build

---
+++ PersonalCRM/build/lib/CMakeFiles/personalCrmLib.dir/employee.cpp.o.d
@@ -0,0 +1,3 @@
+lib/CMakeFiles/personalCrmLib.dir/employee.cpp.o: \
+ /home/vilya/Programming/C++/GtkmmExamples/PersonalCRM/lib/employee.cpp \
+ /usr/include/stdc-predef.h
---
+++ PersonalCRM/build/src/CMakeFiles/personalCrm.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/personalCrm.dir/gresources.c.o"
+  "CMakeFiles/personalCrm.dir/gresources.c.o.d"
+  "CMakeFiles/personalCrm.dir/main-window.cpp.o"
+  "CMakeFiles/personalCrm.dir/main-window.cpp.o.d"
+  "CMakeFiles/personalCrm.dir/main.cpp.o"
+  "CMakeFiles/personalCrm.dir/main.cpp.o.d"
+  "gresources.c"
+  "personalCrm"
+  "personalCrm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/personalCrm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

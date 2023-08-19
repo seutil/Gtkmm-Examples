bac1864452e916714da160781e383796bdf54b25
seutil <seutil@inbox.ru>
2023-08-19 13:03:21 +0300

    Fixed country, region, city comboboxes

---
+++ PersonalCRM/src/main-window.cpp
@@ -0,0 +1,210 @@
+#include <unordered_map>
+#include "main-window.hpp"
+
+const std::unordered_map<std::string, std::unordered_map<std::string, std::vector<std::string>>> COUNTRIES_REGIONS_CITIES =
+{
+    { "Россия", {
+        { "Краснодарский край", { "Краснодар", "Хадыжинск", "Сочи", "Ейск", "Апшеронск" } },
+        { "Алтайский край", { "Барнаул", "Бийск", "Горняк" } },
+        { "Камчатский край", { "Петропавловск-Камчатский", "Елизово", "Вилючинск" } },
+    }},
+    { "Беларусь", {
+        { "Мінская вобласць", { "Мінск", "Заслаўе", "Смалявічы", "Фаніпаль" } }, 
+        { "Віцебская вобласць", { "Віцебск", "Гарадок", "Глыбокае", "Сянно" } },
+        { "Магілёўская вобласць", { "Магілёў", "Гаі", "Быхаў", "Гарадшчына" } }
+    }},
+    { "Україна", {
+        { "Одеська область", { "Одеса", "Южне", "Білгород-Дністровський", "Подільськ" } },
+        { "Дніпропетровська область", { "Дніпро", "П'ятихатки", "Зеленодольськ", "Новомосковськ" } },
+        { "Чернігівська область", { "Чернігів", "Прилуки", "Мена", "Корюківка" } },
+        { "Харківська область", { "Харків", "Лозова", "Ізюм", "Ку́п'янськ" } }
+    }},
+    { "საქართველო", {
+        { "აჭარა", { "ბათუმი", "ქობულეთი", "ხულო", "შუახევი" } },
+        { "გურია", { "ოზურგეთი", "ლანჩხუთი", "ქვედა ნასაკირალი", "ჯაპანა" } },
+        { "სამეგრელო-ზემო სვანეთი", { "ზუგდიდი", "ანაკლია", "რუხი", "უშგული" } }
+    }},
+    { "Հայաստան", {
+        { "Արմավիրի մարզ", { "Վաղարշապատ", "Արաքս", "Հայկաշեն", "Քարակերտ" } },
+        { "Շիրակի մարզ", { "Գյումրի", "Մարալիկ", "Արթիկ", "Ամասիա" } },
+        { "Արագածոտնի մարզ", { "Աշտարակ", "Սասունիկ", "Կարբի", "Փարպի" } }
+    }},
+    { "Қазақстан", {
+        { "Ақмола облысы", { "Атбасар", "Мариновка", "Сергеевка", "Ақан Құрманов" } },
+        { "Алматы облысы", { "Қонаев", "Ерейментау", "Ақкө́л", "Көкшетау" } },
+        { "Ақтөбе облысы", { "Ақтөбе", "Алға", "Талғар", "Алмалық" } }
+    }}
+};
+
+MainWindow::MainWindow(BaseObjectType* c_object,
+                       const Glib::RefPtr<Gtk::Builder>& builder)
+    : Gtk::ApplicationWindow(c_object)
+{
+    builder->get_widget<Gtk::Box>("box_information", m_box_information);
+    builder->get_widget<Gtk::RadioButton>("rad_male", m_rad_male);
+    builder->get_widget<Gtk::RadioButton>("rad_female", m_rad_female);
+    builder->get_widget<Gtk::Entry>("entry_name", m_entry_name);
+    builder->get_widget<Gtk::Entry>("entry_surname", m_entry_surname);
+    builder->get_widget<Gtk::Entry>("entry_patronomic", m_entry_patronomic);
+    builder->get_widget<Gtk::Entry>("entry_phone", m_entry_phone);
+    builder->get_widget<Gtk::Entry>("entry_email", m_entry_email);
+    builder->get_widget<Gtk::Entry>("entry_supervisor", m_entry_supervisor);
+    builder->get_widget<Gtk::ComboBoxText>("cmbt_country", m_cmbt_country);
+    builder->get_widget<Gtk::ComboBoxText>("cmbt_region", m_cmbt_region);
+    builder->get_widget<Gtk::ComboBoxText>("cmbt_city", m_cmbt_city);
+    builder->get_widget<Gtk::ComboBoxText>("cmbt_department", m_cmbt_department);
+    builder->get_widget<Gtk::ComboBoxText>("cmbt_position", m_cmbt_position);
+    builder->get_widget<Gtk::SpinButton>("spin_salary", m_spin_salary);
+    builder->get_widget<Gtk::TextView>("tview_notes", m_tview_notes);
+    builder->get_widget<Gtk::Button>("btn_back", m_btn_back);
+    builder->get_widget<Gtk::Button>("btn_forward", m_btn_forward);
+    builder->get_widget<Gtk::Button>("btn_add", m_btn_add);
+    builder->get_widget<Gtk::Button>("btn_remove", m_btn_remove);
+    builder->get_widget<Gtk::Button>("btn_save", m_btn_save);
+    builder->get_widget<Gtk::Button>("btn_quit", m_btn_quit);
+
+    m_cmbt_country->signal_changed().connect(sigc::mem_fun(*this, &MainWindow::on_cmbt_country_changed));
+    m_con_cmbt_region = m_cmbt_region->signal_changed().connect(sigc::mem_fun(*this, &MainWindow::on_cmbt_region_changed));
+    m_cmbt_city->signal_changed().connect(sigc::mem_fun(*this, &MainWindow::on_cmbt_city_changed));
+    m_btn_quit->signal_clicked().connect(sigc::mem_fun(*this, &MainWindow::close));
+    m_btn_add->signal_clicked().connect(sigc::mem_fun(*this, &MainWindow::on_btn_add_clicked));
+
+    m_crm.load("personal.xml");
+    display_employee();
+}
+
+void
+MainWindow::display_employee()
+{
+    if (m_crm.employees().empty())
+    {
+        m_box_information->set_visible(false);
+        m_btn_back->set_sensitive(false);
+        m_btn_forward->set_sensitive(false);
+        m_btn_remove->set_sensitive(false);
+        return;
+    }
+
+    Lib::Employee& e = m_crm.employee();
+    m_entry_name->set_text(e.name);
+    m_entry_surname->set_text(e.surname);
+    m_entry_patronomic->set_text(e.patronomic);
+    m_entry_phone->set_text(e.phone);
+    m_entry_email->set_text(e.email);
+    m_entry_supervisor->set_text(e.supervisor);
+    if (e.country.empty())
+        m_cmbt_country->set_active_id("-1");
+    else
+        m_cmbt_country->set_active_text(e.country);
+
+    if (e.region.empty())
+        m_cmbt_region->set_active_id("-1");
+    else
+        m_cmbt_region->set_active_text(e.region);
+
+    if (e.city.empty())
+        m_cmbt_city->set_active_id("-1");
+    else
+        m_cmbt_city->set_active_text(e.city);
+
+    m_cmbt_department->set_active_text(e.department);
+    m_cmbt_position->set_active_text(e.position);
+    m_spin_salary->set_value(e.salary);
+    m_tview_notes->get_buffer()->set_text(e.notes);
+
+    m_box_information->set_visible();
+    m_btn_remove->set_sensitive();
+    m_btn_forward->set_sensitive(m_crm.have_next());
+    m_btn_back->set_sensitive(m_crm.have_previouse());
+}
+
+bool
+MainWindow::valid_employee_data()
+{
+    if (m_crm.employees().empty())
+        return true;
+    
+    Lib::Employee& e = m_crm.employee();
+    return !e.name.empty() &&
+           !e.surname.empty() &&
+           !e.patronomic.empty();
+}
+
+// Handlers
+void
+MainWindow::on_cmbt_country_changed()
+{
+    m_con_cmbt_region.block();
+    m_cmbt_region->remove_all();
+    m_cmbt_city->remove_all();
+    if (m_cmbt_country->get_active_id() == "-1")
+    {
+        m_crm.employee().country.clear();
+        m_crm.employee().region.clear();
+        m_crm.employee().city.clear();
+        m_cmbt_region->set_sensitive(false);
+        m_cmbt_city->set_sensitive(false);
+        return;
+    }
+
+    m_crm.employee().country = m_cmbt_country->get_active_text();
+    m_cmbt_region->set_sensitive();
+    m_cmbt_city->set_sensitive();
+
+    // populate regions
+    int id = -1;
+    m_cmbt_region->append(Glib::ustring::format(id++), "Unknown");
+    Glib::ustring country = m_cmbt_country->get_active_text();
+    for (const auto& r : COUNTRIES_REGIONS_CITIES.at(country))
+        m_cmbt_region->append(Glib::ustring::format(id++), r.first);
+
+    m_cmbt_region->set_active_id("-1");
+    m_cmbt_city->set_active_id("-1");
+    m_con_cmbt_region.unblock();
+}
+
+void
+MainWindow::on_cmbt_region_changed()
+{
+    m_cmbt_city->remove_all();
+    if (m_cmbt_region->get_active_id() == "-1")
+    {
+        m_crm.employee().region.clear();
+        m_crm.employee().city.clear();
+        m_cmbt_city->set_sensitive(false);
+        return;
+    }
+
+    m_crm.employee().region = m_cmbt_region->get_active_text();
+    m_cmbt_city->set_sensitive();
+    m_cmbt_city->remove_all();
+
+    // populate cities
+    int id = -1;
+    m_cmbt_city->append(Glib::ustring::format(id++), "Unknown");
+    std::string country = m_cmbt_country->get_active_text().raw();
+    std::string region = m_cmbt_region->get_active_text().raw();
+    for (const auto& c : COUNTRIES_REGIONS_CITIES.at(country).at(region))
+        m_cmbt_city->append(Glib::ustring::format(id++), c);
+
+    m_cmbt_city->set_active_id("-1");
+}
+
+void
+MainWindow::on_cmbt_city_changed()
+{
+    if (m_cmbt_city->get_active_id() == "-1")
+        m_crm.employee().city.clear();
+    else
+        m_crm.employee().city = m_cmbt_city->get_active_text();
+}
+
+void
+MainWindow::on_btn_add_clicked()
+{
+    if (!valid_employee_data())
+        return;
+    
+    m_crm.add_employee();
+    display_employee();
+}
---
+++ PersonalCRM/src/main-window.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <personal-crm.hpp>
+#include <gtkmm.h>
+
+class MainWindow : public Gtk::ApplicationWindow
+{
+public:
+    MainWindow(BaseObjectType* c_object,
+               const Glib::RefPtr<Gtk::Builder>& builder);
+private:
+    Lib::PersonalCrm m_crm;
+    void display_employee();
+    bool valid_employee_data();
+
+    // Handlers
+    void on_cmbt_country_changed();
+    void on_cmbt_region_changed();
+    void on_cmbt_city_changed();
+    void on_btn_add_clicked();
+
+    // Widgets
+    Gtk::Box* m_box_information;
+
+    // Personal information
+    Gtk::Image* m_img_avatar;
+    Gtk::RadioButton* m_rad_male;
+    Gtk::RadioButton* m_rad_female;
+    Gtk::Entry* m_entry_name;
+    Gtk::Entry* m_entry_surname;
+    Gtk::Entry* m_entry_patronomic;
+    Gtk::Entry* m_entry_phone;
+    Gtk::Entry* m_entry_email;
+    Gtk::ComboBoxText* m_cmbt_country;
+    Gtk::ComboBoxText* m_cmbt_region;
+    Gtk::ComboBoxText* m_cmbt_city;
+
+    // Employee information
+    Gtk::Entry* m_entry_supervisor;
+    Gtk::ComboBoxText* m_cmbt_department;
+    Gtk::ComboBoxText* m_cmbt_position;
+    Gtk::SpinButton* m_spin_salary;
+    
+    // Additional information
+    Gtk::TextView* m_tview_notes;
+
+    // Button controlls
+    Gtk::Button* m_btn_back;
+    Gtk::Button* m_btn_forward;
+    Gtk::Button* m_btn_remove;
+    Gtk::Button* m_btn_add;
+    Gtk::Button* m_btn_save;
+    Gtk::Button* m_btn_quit;
+
+    sigc::connection m_con_cmbt_region;
+};
